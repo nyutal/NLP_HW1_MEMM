4c525b4520246f594ac6eed1479cfa25a18c1f5b
hillelme <hillelme@gmail.com>
2016-12-09 20:41:44 +0200

    parsed sentebces and saved dictionary of trigrams, bigrams, emmision to be used as  indexes for weight vector

--- main.py
+++ main.py
@@ -32,93 +32,66 @@ def main():
     sentences = []
     sentences_w = []
     sentences_t = []
-    word_count = {}
-    tag_count = {}
+
 
     lines = [line.rstrip('\n') for line in open('train_small.wtag')]
     # lines = [line.rstrip('\n') for line in open('train.wtag')]
 
+    prepare_features(lines, sentences, sentences_t, sentences_w)
+
+    logging.info('Done!')
+    print("Done!")
+
+
+def prepare_features(lines, sentences, sentences_t, sentences_w):
     for line in lines:
-        w = ['*_*', '*_*'] # start
+        w = ['*_*', '*_*']  # start
         w.extend(line.split(" "))
         if w[-1] == '._.':
-            del w[-1] # remove 'period' from the end of the sentence
-        w.append('SSS_SSS') # stop
+            del w[-1]  # remove 'period' from the end of the sentence
+        w.append('SSS_SSS')  # stop
         sentences.append(w)
-
     for sentence in sentences:
         w = []
         tag = []
         for word in sentence:
-            a,b = word.split("_")
+            a, b = word.split("_")
             w.append(a)
             tag.append(b)
         sentences_w.append(w)
         sentences_t.append(tag)
-
     print(sentences_w[0])
     print(sentences_t[0])
-
     # add a feature for each trigram seen in the training data
     features = {}
     v_index = 0
     bigrams_offset = 0
     for sentence_t in sentences_t:
-        for i in range(len(sentence_t)-2):
-            t_hash = hash((sentence_t[i], sentence_t[i+1], sentence_t[i+2])) #TODO: make all index backards (n,n-1,n-2)
+        for i in range(len(sentence_t) - 2):
+            t_hash = hash(
+                (sentence_t[i], sentence_t[i + 1], sentence_t[i + 2]))  # TODO: make all index backards (n,n-1,n-2)
             if t_hash not in features:
                 features[t_hash] = v_index
                 v_index += 1
     bigrams_offset = v_index
     print(bigrams_offset)
-
     # add a feature for each bigram seen in the training data
     for sentence_t in sentences_t:
-        for i in range(len(sentence_t)-1):
+        for i in range(len(sentence_t) - 1):
             t_hash = hash((sentence_t[i], sentence_t[i + 1]))
             if t_hash not in features:
                 features[t_hash] = v_index
                 v_index += 1
     unigrams_offset = v_index
     print(unigrams_offset)
-
     for sentence_w, sentence_t in zip(sentences_w, sentences_t):
-        for i in range(len(sentence_t)-1):
+        for i in range(len(sentence_t) - 1):
             t_hash = hash((sentence_w[i], sentence_t[i]))
             if t_hash not in features:
                 features[t_hash] = v_index
                 v_index += 1
     print(v_index)
 
-    # for line in sentences:
-    #     for word in line:
-    #         w,tag = word.split("_")
-    #         lower_w = str(w).lower()
-    #         if lower_w in word_count:
-    #             word_count[lower_w] += 1
-    #         else:
-    #             word_count[lower_w] = 1
-    #         if tag in tag_count:
-    #             tag_count[tag] += 1
-    #         else:
-    #             tag_count[tag] = 1
-            # print(lower_w,tag)
-
-    # print()
-    # for tri in trigrams:
-    #     print(tri)
-
-    # capital_word = 'Word'
-    # print('capital_word?: ', capital_word.islower())
-
-    # print('number of uniqe words: ' , len(word_count))
-    # print(tag_count)
-    # print('number of uniqe tags: ' , len(tag_count))
-
-    # for w in tag_count:
-    #     print('the tag ', w, ' appears: ', tag_count[w], ' times')
-    logging.info('Done!')
-    print("Done!")
 
 """Run main"""
 if __name__ == '__main__':
